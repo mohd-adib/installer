2cb296e95ef2598084305eaf4d0ccdbf0f681354
Mohd Adib <relesafe@gmail.com>
2018-01-14 11:27:53 +0800

    update

--- all.sh
+++ all.sh
@@ -1,7 +1,6 @@
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
-
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 #rbenv and rails
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
